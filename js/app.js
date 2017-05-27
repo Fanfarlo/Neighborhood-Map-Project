bc14bcb8cf9abcc75b020e58797b64472ac64cab
Fanfarlo <miguelo.mgmt@gmail.com>
2017-05-27 13:07:30 -0500

    Fixed google is not defined

--- js/app.js
+++ js/app.js
@@ -208,12 +208,7 @@ class AppViewModel {
     });
   }
 
-  // Style the markers a bit. This will be our listing marker icon.
-  var defaultIcon = makeMarkerIcon('0091ff');
 
-  // Create a "highlighted location" marker color for when the user
-  // mouses over the marker.
-  var highlightedIcon = makeMarkerIcon('FFFF24');
 
   // Create and place markers and info windows on the map based on data from API
   function markers(array) {
@@ -281,19 +276,6 @@ class AppViewModel {
     self.markers([]);
   }
 
-  // This function takes in a COLOR, and then creates a new marker
-  // icon of that color. The icon will be 21 px wide by 34 high, have an origin
-  // of 0, 0 and be anchored at 10, 34).
-  function makeMarkerIcon(markerColor) {
-    var markerImage = new google.maps.MarkerImage(
-      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
-      '|40|_|%E2%80%A2',
-      new google.maps.Size(21, 34),
-      new google.maps.Point(0, 0),
-      new google.maps.Point(10, 34),
-      new google.maps.Size(21, 34));
-    return markerImage;
-  }
 
   function toggleBounce(marker) {
     if (marker.getAnimation() !== null) {
@@ -492,8 +474,29 @@ initMap = () => {
   infowindow = new google.maps.InfoWindow({
     maxWidth: 300
   });
+
   getGroupons('Vancouver');
   getGrouponLocations();
+
+  // This function takes in a COLOR, and then creates a new marker
+  // icon of that color. The icon will be 21 px wide by 34 high, have an origin
+  // of 0, 0 and be anchored at 10, 34).
+  function makeMarkerIcon(markerColor) {
+    var markerImage = new google.maps.MarkerImage(
+      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+      '|40|_|%E2%80%A2',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(10, 34),
+      new google.maps.Size(21, 34));
+    return markerImage;
+  }
+  // Style the markers a bit. This will be our listing marker icon.
+  var defaultIcon = makeMarkerIcon('0091ff');
+
+  // Create a "highlighted location" marker color for when the user
+  // mouses over the marker.
+  var highlightedIcon = makeMarkerIcon('FFFF24');
 
 };
 
@@ -512,6 +515,7 @@ ko.bindingHandlers.selectOnFocus = {
     });
   }
 };
+
 
 var vm = new AppViewModel();
 ko.applyBindings(vm);
